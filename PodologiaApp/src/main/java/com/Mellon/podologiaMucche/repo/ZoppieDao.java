ac8f058311baa74b8dea951dcf027948d70caee0
Lacrosser <114079266+Lacrosser@users.noreply.github.com>
2023-01-20 13:46:24 +0100

    Update ZoppieDao.java

--- PodologiaApp/src/main/java/com/Mellon/podologiaMucche/repo/ZoppieDao.java
+++ PodologiaApp/src/main/java/com/Mellon/podologiaMucche/repo/ZoppieDao.java
@@ -2,6 +2,6 @@ package com.Mellon.podologiaMucche.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Zoppie extends JpaRepository<Zoppie, Integer> {
+public interface ZoppieDao extends JpaRepository<ZoppieDao, Integer> {
 
 }
